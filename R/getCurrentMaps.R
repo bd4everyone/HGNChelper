9d678308c6e5aa3b97df60e0b7e98bf1fd021cb8
Levi Waldron <lwaldron.research@gmail.com>
2024-05-17 17:16:29 -0700

    Separate download from read, check # rows

--- R/getCurrentMaps.R
+++ R/getCurrentMaps.R
@@ -7,7 +7,7 @@
 #' Make sure to change the default \code{species="human"} argument to \code{checkGeneSymbols} 
 #' if you are doing this for mouse. Use \code{getCurrentHumanMap} for HGNC human gene 
 #' symbols from \url{genenames.org} and \code{getCurrentMouseMap} for MGI mouse gene 
-#' symbols from \url{www.informatics.jax.org/downloads/reports/MGI_EntrezGene.rpt}.
+#' symbols from \url{https://www.informatics.jax.org/downloads/reports/MGI_EntrezGene.rpt}.
 #' @return A \code{data.frame} that can be used for \code{map} argument of \code{checkGeneSymbols} function
 #' @export getCurrentMouseMap getCurrentHumanMap
 #' @usage 
@@ -16,6 +16,7 @@
 #' @importFrom stats complete.cases
 #' @importFrom utils read.delim
 #' @importFrom splitstackshape cSplit
+#' @importFrom utils download.file
 #' @examples
 #' \dontrun{
 #' ## human
@@ -52,8 +53,18 @@ getCurrentHumanMap <- function(){
     return(hgnc.table)
   }
   url <- "ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt"
-  message(paste("Fetching gene symbols from", url))
-  map <- read.delim(url, as.is=TRUE)
+  if(!file.exists("hgnc_complete_set.txt")){
+    message(paste("Fetching human gene symbols from", url))
+    download.file(url, destfile = "hgnc_complete_set.txt")    
+  }else{
+    message("Using the already downloaded hgnc_complete_set.txt file.")
+  }
+  map <- read.delim("hgnc_complete_set.txt", as.is=TRUE)
+  if(nrow(map) < 40000){
+    warning("The hgnc_complete_set.txt file has fewer than 40,000 rows.",
+            "As of May 17 2014, it already had 43,842 rows, so the download was probably incomplete.", 
+            "Please check the download.")
+  }  
   # 2 = symbol, 3=alias_symbol, 4=prev_symbol, 7=location
   # corrections
   has.corrections <- nchar(map$alias_symbol)>0 | nchar(map$prev_symbol)>0
@@ -95,8 +106,19 @@ getCurrentMouseMap <- function(){
   fname <- system.file("extdata/HGNChelper_mog_map_MGI_AMC_2016_03_30.csv", package = "HGNChelper")
   O = read.csv(fname, as.is=TRUE)[, 2:1]
   colnames(O) <- c("Symbol", "Approved.Symbol")
-  
-  map <- read.delim("http://www.informatics.jax.org/downloads/reports/MGI_EntrezGene.rpt", as.is=TRUE, header = FALSE)
+  url <- "https://www.informatics.jax.org/downloads/reports/MGI_EntrezGene.rpt"
+  if(!file.exists("MGI_EntrezGene.rpt")){
+    message(paste("Fetching gene symbols from", url))
+    download.file(url, destfile = "MGI_EntrezGene.rpt")    
+  }else{
+    message("Using the already downloaded MGI_EntrezGene.rpt file.")
+  }
+  map <- read.delim("MGI_EntrezGene.rpt", as.is=TRUE, header = FALSE)
+  if(nrow(map) < 700000){
+    warning("The MGI_EntrezGene.rpt file has fewer than 700,000 rows.",
+    "As of May 17 2014, it already had 752,802 rows, so the download was probably incomplete.", 
+    "Please check the download.")
+  }
   map <- map[, 2:4]
   
   map.ok <- map[grep("^O$", map[, 2]), c(1, 3)]
